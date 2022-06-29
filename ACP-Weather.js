0b66c2e0648eab27fae588e377393d1f0ae08e69
mjmazur <mmazur5@uwo.ca>
2022-06-29 14:22:40 -0400

    Add files via upload

---
+++ ACP-Weather.js
@@ -0,0 +1,88 @@
+var SUP;
+
+//////////////////////////////
+// Returns date as yyyymmdd
+// MJM - June 2021
+//////////////////////////////
+
+function getDate()
+{
+	var d, s, month, day;
+	d = new Date();
+
+	s = d.getUTCFullYear();
+
+	if (d.getUTCMonth().toString().length == 1)
+	{
+		s += "0" + (d.getUTCMonth()+1);
+	}
+	else
+	{
+		s += (d.getUTCMonth()+1);
+	}
+
+	if (d.getUTCDate().toString().length == 1)
+	{
+		s += "0" + d.getUTCDate();
+	}
+	else
+	{
+		s += d.getUTCDate()
+	}
+	return(s)
+}
+
+function main()
+{
+	var fso, f1, ts;
+	var Mode = 8;
+
+	LogFile = "d:\\Logs\\ACP\\" + getDate() + "-ACP.log";
+
+	fso = new ActiveXObject("Scripting.FileSystemObject");
+
+	if (fso.FileExists(LogFile))
+	{
+		Console.PrintLine("Log file exists. Appending to existing.")
+	}
+	else
+	{
+		fso.CreateTextFile(LogFile);
+	}
+
+	f1 = fso.GetFile(LogFile);
+	ts = f1.OpenAsTextStream(Mode, true);
+
+	Console.PrintLine(Util.SysUTCDate + " ALERT: Weather Safety Script Activated!");
+	// ts.WriteBlankLines(2);
+	ts.WriteLine(Util.SysUTCDate + " ALERT: ####################################");
+	ts.WriteLine(Util.SysUTCDate + " ALERT: # Weather Safety Script Activated! #");
+	ts.WriteLine(Util.SysUTCDate + " ALERT: ####################################");
+
+	Console.PrintLine("Closing dome...");
+	ts.WriteLine(Util.SysUTCDate + " ALERT: Closing dome...");
+
+	Dome.CloseShutter();
+	Console.PrintLine("Dome closed!");
+	ts.WriteLine(Util.SysUTCDate + " ALERT: Dome closed!");
+
+	if (Telescope.Connected)
+	{
+		Console.PrintLine("Parking telescope...");
+		ts.WriteLine(Util.SysUTCDate + " ALERT: Parking telescope...");
+
+		Telescope.Park();
+		Console.PrintLine("Telescope parked!");
+		ts.WriteLine(Util.SysUTCDate + " ALERT: Telescope parked!");
+	}
+
+	Console.PrintLine("Restarting scheduler...")
+	// ts.WriteBlankLines(2)
+	ts.WriteLine(Util.SysUTCDate + " ALERT: ###########################");
+	ts.WriteLine(Util.SysUTCDate + " ALERT: # Restarting scheduler... #")
+	ts.WriteLine(Util.SysUTCDate + " ALERT: ###########################");
+	// ts.WriteBlankLines(2);
+
+	Util.ChainScript("C:\\Users\\RedBird\\Documents\\ACP Astronomy\\Scripts\\RunColibri.js");
+	Util.WaitForMilliseconds(3000);
+}
