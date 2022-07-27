6d00a94252ce456e5f853fdd52af68787e5eb091
Michael Mazur <mjmazur@gmail.com>
2022-07-27 12:53:55 -0400

    Update PyFocus.py

--- PyFocus/PyFocus.py
+++ PyFocus/PyFocus.py
@@ -23,234 +23,234 @@ from PyQt5.QtGui import *
 
 class FocusThread(QtCore.QThread):
 
-    def __init__(self,parent=None):
-        super(FocusThread,self).__init__(parent)
-        self.threadactive = True
+	def __init__(self,parent=None):
+		super(FocusThread,self).__init__(parent)
+		self.threadactive = True
 
-    def run(self):
-        while self.threadactive:
-            # print('Hello World')
-            # time.sleep(1)
-            self.image = self.grabImage.emit(0,0,self.Zoom_slider.value()*50,self.Zoom_slider.value()*50,self.Exposure_spinbox.value())
-            self.updateFocusFrame.emit(self.image)
+	def run(self):
+		while self.threadactive:
+			# print('Hello World')
+			# time.sleep(1)
+			self.image = self.grabImage.emit(0,0,self.Zoom_slider.value()*50,self.Zoom_slider.value()*50,self.Exposure_spinbox.value())
+			self.updateFocusFrame.emit(self.image)
 
-    def stop(self):
-        self.threadactive = False
-        self.wait()
+	def stop(self):
+		self.threadactive = False
+		self.wait()
 
 class Ui(QtWidgets.QMainWindow):
 
-    # emit_stop = QtCore.pyqtSignal(int)
+	# emit_stop = QtCore.pyqtSignal(int)
 
-    def __init__(self, *args, **kwargs):
+	def __init__(self, *args, **kwargs):
 
-        # call inherited classes __init__ method
-        super(Ui, self).__init__(*args, **kwargs)    
-        
-        # Load the .ui file
-        uic.loadUi('PyFocus.ui', self)                
-        self.title = "PyFocus Automated Focusing"
-
-
-        width = 320
-        height = 785
-        # setting  the fixed size of window
-        self.setFixedSize(width, height)
-
-        # display the GUI 
-        self.show()
-
-        ####### GUI Modifications
-        self.focus_imagewidget = pg.ImageView()
-        self.Focus_layout.addWidget(self.focus_imagewidget, 0, 0)
-        self.focus_imagewidget.show()
-        self.focus_imagewidget.ui.histogram.hide()
-        self.focus_imagewidget.ui.roiBtn.hide()
-        self.focus_imagewidget.ui.menuBtn.hide()
-
-        ##### Button triggers
-        self.Start_button.clicked.connect(self.startFocus)
-        self.Stop_button.clicked.connect(self.stopFocus)
-
-        # self.JogNorth_button.clicked.connect(self.jogScope)
-        # self.JogSouth_button.clicked.connect(self.jogScope)
-        # self.JogEast_button.clicked.connect(self.jogScope)
-        # self.JogWest_button.clicked.connect(self.jogScope)
-        
-
-        # self.Plot.setXRange(0,1)
-
-        self.plot([1,2,3,4,5,6,7,8,9,10], [30,32,34,32,33,31,29,32,35,45])
-
-        self.thread = FocusThread(self)
+		# call inherited classes __init__ method
+		super(Ui, self).__init__(*args, **kwargs)    
+		
+		# Load the .ui file
+		uic.loadUi('PyFocus.ui', self)                
+		self.title = "PyFocus Automated Focusing"
 
 
-    def watchthread(self,worker):
-        self.thread = worker(self)
-        self.thread.grabImage.connect(self.grabImage)
-        self.thread.updateFocusFrame.connect(self.updateFocusFrame)
+		width = 320
+		height = 785
+		# setting  the fixed size of window
+		self.setFixedSize(width, height)
 
-        # self.thread.finished.connect(self.close)
+		# display the GUI 
+		self.show()
 
-    def startthread(self):
-        self.thread.start()
+		####### GUI Modifications
+		self.focus_imagewidget = pg.ImageView()
+		self.Focus_layout.addWidget(self.focus_imagewidget, 0, 0)
+		self.focus_imagewidget.show()
+		self.focus_imagewidget.ui.histogram.hide()
+		self.focus_imagewidget.ui.roiBtn.hide()
+		self.focus_imagewidget.ui.menuBtn.hide()
 
-    def killthread(self):
-        self.thread.stop()
-        print('Say what?')
+		##### Button triggers
+		self.Start_button.clicked.connect(self.startFocus)
+		self.Stop_button.clicked.connect(self.stopFocus)
 
-    def plot(self, hour, temperature):
-        labelStyle = {'color': '#FFF', 'font-size': '12px', 'padding': '0px'}
-        self.Plot.plot(hour, temperature)
-        self.Plot.setLabel('left', 'PSF (px)', **labelStyle)
-        self.Plot.autoRange(padding=0)
-        # print(self.Plot.ViewBox.screenGeometry())
+		# self.JogNorth_button.clicked.connect(self.jogScope)
+		# self.JogSouth_button.clicked.connect(self.jogScope)
+		# self.JogEast_button.clicked.connect(self.jogScope)
+		# self.JogWest_button.clicked.connect(self.jogScope)
+		
 
-        # self.Plot.plotItem.getViewBox().setBackgroundColor((192, 192, 192))
-        # self.Plot.setLabel('bottom', 'hour', **labelStyle)
+		# self.Plot.setXRange(0,1)
 
-        #######              
+		self.plot([1,2,3,4,5,6,7,8,9,10], [30,32,34,32,33,31,29,32,35,45])
 
-    def connectDevices(self):
-        try:
-            T.Connected = True
-            print('Connected to telescope...')
-            C.Connected = True
-            print('Connected to camera...')
-        except Exception as e:
-            print(f'ERROR:  {str(e)}')
+		self.thread = FocusThread(self)
 
 
+	def watchthread(self,worker):
+		self.thread = worker(self)
+		self.thread.grabImage.connect(self.grabImage)
+		self.thread.updateFocusFrame.connect(self.updateFocusFrame)
 
-    def changeFocus(step,dir):
-        # Adjust focus
-        currPos = F.Position
-        if dir == 'in':
-            F.Position = currPos + step
-        elif dir == 'out':
-            F.Position = currPos - step
+		# self.thread.finished.connect(self.close)
 
-    def slewToAltAz(alt,az):
-        T.SlewToAltAz()
+	def startthread(self):
+		self.thread.start()
 
-    def startFocus(self):
-        self.connectDevices()
-        self.image = self.grabImage(0,0,self.Zoom_slider.value()*50,self.Zoom_slider.value()*50,self.Exposure_spinbox.value())
+	def killthread(self):
+		self.thread.stop()
+		print('Say what?')
 
-        self.updateFocusFrame(self.image)
+	def plot(self, hour, temperature):
+		labelStyle = {'color': '#FFF', 'font-size': '12px', 'padding': '0px'}
+		self.Plot.plot(hour, temperature)
+		self.Plot.setLabel('left', 'PSF (px)', **labelStyle)
+		self.Plot.autoRange(padding=0)
+		# print(self.Plot.ViewBox.screenGeometry())
 
-        self.watchthread(FocusThread)
-        self.startthread()
+		# self.Plot.plotItem.getViewBox().setBackgroundColor((192, 192, 192))
+		# self.Plot.setLabel('bottom', 'hour', **labelStyle)
 
-        # print(self.Start_button.isChecked())
-        # if self.Start_button.isChecked():
-        #     self.watchthread(FocusThread)
-        #     self.startthread()
+		#######              
 
-        #     # print(self.ctrl)
-        #     # self.ctrl['break'] = False
-        #     # self.start()
-        # else:
-        #     self.killthread()
+	def connectDevices(self):
+		try:
+			T.Connected = True
+			print('Connected to telescope...')
+			C.Connected = True
+			print('Connected to camera...')
+		except Exception as e:
+			print(f'ERROR:  {str(e)}')
 
-    def stopFocus(self):
-        self.killthread()
 
-    def grabImage(x,y,sizex,sizey, exposure):
-    C.StartX = x
-    C.StartY = y
-    C.NumX = sizex
-    C.NumY = sizey
 
-    C.StartExposure(exposure, True)
-    while not C.ImageReady:
-        time.sleep(0.5)
-        print(f'{C.PercentCompleted}% complete')
-    print('finished')
+	def changeFocus(step,dir):
+		# Adjust focus
+		currPos = F.Position
+		if dir == 'in':
+			F.Position = currPos + step
+		elif dir == 'out':
+			F.Position = currPos - step
 
-    img = c.ImageArray
-    imginfo = c.ImageArrayInfo
-    if imginfo.ImageElementType == ImageArrayElementTypes.Int32:
-        if c.MaxADU <= 65535:
-            imgDataType = np.uint16 # Required for BZERO & BSCALE to be written
-        else:
-            imgDataType = np.int32
-    elif imginfo.ImageElementType == ImageArrayElementTypes.Double:
-        imgDataType = np.float64
-    #
-    # Make a numpy array of he correct shape for astropy.io.fits
-    #
-    if imginfo.Rank == 2:
-        nda = np.array(img, dtype=imgDataType).transpose()
-    else:
-        nda = np.array(img, dtype=imgDataType).transpose(2,1,0)
+	def slewToAltAz(alt,az):
+		T.SlewToAltAz()
 
-    return nda
+	def startFocus(self):
+		self.connectDevices()
+		self.image = self.grabImage(0,0,self.Zoom_slider.value()*50,self.Zoom_slider.value()*50,self.Exposure_spinbox.value())
 
-    def updateFocusFrame(self, image):
+		self.updateFocusFrame(self.image)
 
-        self.focus_imagewidget.setImage(image, levels=(50,200))
-        # self.focus_imagewidget.autoRange()
+		self.watchthread(FocusThread)
+		self.startthread()
 
-    def readxbytes(fid, numbytes):
-        for i in range(1):
-            data = fid.read(numbytes)
-            if not data:
-                break
-        return data
+		# print(self.Start_button.isChecked())
+		# if self.Start_button.isChecked():
+		#     self.watchthread(FocusThread)
+		#     self.startthread()
 
-    @nb.njit(nb.uint16[::1](nb.uint8[::1]),fastmath=True,parallel=True)
-    def nb_read_data(data_chunk):
-        """data_chunk is a contigous 1D array of uint8 data)
-        eg.data_chunk = np.frombuffer(data_chunk, dtype=np.uint8)"""
-        #ensure that the data_chunk has the right length
+		#     # print(self.ctrl)
+		#     # self.ctrl['break'] = False
+		#     # self.start()
+		# else:
+		#     self.killthread()
 
-        assert np.mod(data_chunk.shape[0],3)==0
+	def stopFocus(self):
+		self.killthread()
 
-        out=np.empty(data_chunk.shape[0]//3*2,dtype=np.uint16)
-        image1 = np.empty((2048,2048),dtype=np.uint16)
-        image2 = np.empty((2048,2048),dtype=np.uint16)
+	def grabImage(x,y,sizex,sizey, exposure):
+	C.StartX = x
+	C.StartY = y
+	C.NumX = sizex
+	C.NumY = sizey
 
-        for i in nb.prange(data_chunk.shape[0]//3):
-            fst_uint8=np.uint16(data_chunk[i*3])
-            mid_uint8=np.uint16(data_chunk[i*3+1])
-            lst_uint8=np.uint16(data_chunk[i*3+2])
+	C.StartExposure(exposure, True)
+	while not C.ImageReady:
+		time.sleep(0.5)
+		print(f'{C.PercentCompleted}% complete')
+	print('finished')
 
-            out[i*2] =   (fst_uint8 << 4) + (mid_uint8 >> 4)
-            out[i*2+1] = ((mid_uint8 % 16) << 8) + lst_uint8
+	img = c.ImageArray
+	imginfo = c.ImageArrayInfo
+	if imginfo.ImageElementType == ImageArrayElementTypes.Int32:
+		if c.MaxADU <= 65535:
+			imgDataType = np.uint16 # Required for BZERO & BSCALE to be written
+		else:
+			imgDataType = np.int32
+	elif imginfo.ImageElementType == ImageArrayElementTypes.Double:
+		imgDataType = np.float64
+	#
+	# Make a numpy array of he correct shape for astropy.io.fits
+	#
+	if imginfo.Rank == 2:
+		nda = np.array(img, dtype=imgDataType).transpose()
+	else:
+		nda = np.array(img, dtype=imgDataType).transpose(2,1,0)
 
-        return out
+	return nda
 
-    def split_images(self,data,pix_h,pix_v,gain):
-        interimg = np.reshape(data, [2*pix_v,pix_h])
+	def updateFocusFrame(self, image):
 
-        if gain == 'low':
-            image = interimg[::2]
-        else:
-            image = interimg[1::2]
+		self.focus_imagewidget.setImage(image, levels=(50,200))
+		# self.focus_imagewidget.autoRange()
 
-        return image
+	def readxbytes(fid, numbytes):
+		for i in range(1):
+			data = fid.read(numbytes)
+			if not data:
+				break
+		return data
+
+	@nb.njit(nb.uint16[::1](nb.uint8[::1]),fastmath=True,parallel=True)
+	def nb_read_data(data_chunk):
+		"""data_chunk is a contigous 1D array of uint8 data)
+		eg.data_chunk = np.frombuffer(data_chunk, dtype=np.uint8)"""
+		#ensure that the data_chunk has the right length
+
+		assert np.mod(data_chunk.shape[0],3)==0
+
+		out=np.empty(data_chunk.shape[0]//3*2,dtype=np.uint16)
+		image1 = np.empty((2048,2048),dtype=np.uint16)
+		image2 = np.empty((2048,2048),dtype=np.uint16)
+
+		for i in nb.prange(data_chunk.shape[0]//3):
+			fst_uint8=np.uint16(data_chunk[i*3])
+			mid_uint8=np.uint16(data_chunk[i*3+1])
+			lst_uint8=np.uint16(data_chunk[i*3+2])
+
+			out[i*2] =   (fst_uint8 << 4) + (mid_uint8 >> 4)
+			out[i*2+1] = ((mid_uint8 % 16) << 8) + lst_uint8
+
+		return out
+
+	def split_images(self,data,pix_h,pix_v,gain):
+		interimg = np.reshape(data, [2*pix_v,pix_h])
+
+		if gain == 'low':
+			image = interimg[::2]
+		else:
+			image = interimg[1::2]
+
+		return image
 
 def main():
-    app = QtWidgets.QApplication(sys.argv) # create instance of QtWidgets.QApplication
-    window = Ui()                          # create instance of class
-    app.exec_()                            # start the application
+	app = QtWidgets.QApplication(sys.argv) # create instance of QtWidgets.QApplication
+	window = Ui()                          # create instance of class
+	app.exec_()                            # start the application
 
 T = Telescope('localhost:11111', 0) # Local Omni Simulator
 C = Camera('localhost:11111', 0)
 F = Focuser('localhost:11111', 0)
 
 if __name__ == '__main__':
-    arg_parser = argparse.ArgumentParser(description="""
+	arg_parser = argparse.ArgumentParser(description="""
 
-        """,
-        formatter_class=argparse.RawTextHelpFormatter)
+		""",
+		formatter_class=argparse.RawTextHelpFormatter)
 
-    arg_parser.add_argument('-i', '--interactive', help='Interactive mode', action='store_true')
+	arg_parser.add_argument('-i', '--interactive', help='Interactive mode', action='store_true')
 
-    cml_args = arg_parser.parse_args()
+	cml_args = arg_parser.parse_args()
 
-    if cml_args.interactive is True:
-        main()
-    else:
-        print('non-interactive mode')
+	if cml_args.interactive is True:
+		main()
+	else:
+		print('non-interactive mode')
