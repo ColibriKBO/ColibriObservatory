b078de0a7dd13feb7575a33726b7739c41d235fe
Michael Mazur <mjmazur@gmail.com>
2022-07-27 12:54:32 -0400

    Update PyFocus.py

--- PyFocus/PyFocus.py
+++ PyFocus/PyFocus.py
@@ -157,35 +157,35 @@ class Ui(QtWidgets.QMainWindow):
 		self.killthread()
 
 	def grabImage(x,y,sizex,sizey, exposure):
-	C.StartX = x
-	C.StartY = y
-	C.NumX = sizex
-	C.NumY = sizey
+		C.StartX = x
+		C.StartY = y
+		C.NumX = sizex
+		C.NumY = sizey
 
-	C.StartExposure(exposure, True)
-	while not C.ImageReady:
-		time.sleep(0.5)
-		print(f'{C.PercentCompleted}% complete')
-	print('finished')
+		C.StartExposure(exposure, True)
+		while not C.ImageReady:
+			time.sleep(0.5)
+			print(f'{C.PercentCompleted}% complete')
+		print('finished')
 
-	img = c.ImageArray
-	imginfo = c.ImageArrayInfo
-	if imginfo.ImageElementType == ImageArrayElementTypes.Int32:
-		if c.MaxADU <= 65535:
-			imgDataType = np.uint16 # Required for BZERO & BSCALE to be written
+		img = c.ImageArray
+		imginfo = c.ImageArrayInfo
+		if imginfo.ImageElementType == ImageArrayElementTypes.Int32:
+			if c.MaxADU <= 65535:
+				imgDataType = np.uint16 # Required for BZERO & BSCALE to be written
+			else:
+				imgDataType = np.int32
+		elif imginfo.ImageElementType == ImageArrayElementTypes.Double:
+			imgDataType = np.float64
+		#
+		# Make a numpy array of he correct shape for astropy.io.fits
+		#
+		if imginfo.Rank == 2:
+			nda = np.array(img, dtype=imgDataType).transpose()
 		else:
-			imgDataType = np.int32
-	elif imginfo.ImageElementType == ImageArrayElementTypes.Double:
-		imgDataType = np.float64
-	#
-	# Make a numpy array of he correct shape for astropy.io.fits
-	#
-	if imginfo.Rank == 2:
-		nda = np.array(img, dtype=imgDataType).transpose()
-	else:
-		nda = np.array(img, dtype=imgDataType).transpose(2,1,0)
+			nda = np.array(img, dtype=imgDataType).transpose(2,1,0)
 
-	return nda
+		return nda
 
 	def updateFocusFrame(self, image):
 
