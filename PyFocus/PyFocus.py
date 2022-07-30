e28c9c72a85e69aa98674eebdbd24df6157f8aae
Michael Mazur <mjmazur@gmail.com>
2022-07-29 23:08:35 -0400

    Update PyFocus.py

--- PyFocus/PyFocus.py
+++ PyFocus/PyFocus.py
@@ -461,8 +461,8 @@ class Ui(QtWidgets.QMainWindow):
 
 	def updateFocusFrame(self, image):
 
-		# self.focus_imagewidget.setImage(image, levels=(50,200))
-		self.focus_imagewidget.setImage(image)
+		self.focus_imagewidget.setImage(image, levels=(50,200))
+		# self.focus_imagewidget.setImage(image)
 		# self.focus_imagewidget.autoRange()
 
 	# def readxbytes(fid, numbytes):
