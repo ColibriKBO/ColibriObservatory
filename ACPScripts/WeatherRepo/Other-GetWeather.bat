ae0ab6d450d0575df964c6482d54f78ab0afbf31
pquigle <pquigle@uwo.com>
2022-12-22 14:22:27 -0500

    Changed weather file destination to ProgramFiles(x86)

--- ACPScripts/WeatherRepo/Other-GetWeather.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set /p WeatherName="Name of your weather script: "
-
-copy %WeatherName% ../ACP-Weather.js
-del ../WhichWeatherScript.txt
-echo %WeatherName% > ../WhichWeatherScript.txt
-
-echo Copied over %WeatherName%...
