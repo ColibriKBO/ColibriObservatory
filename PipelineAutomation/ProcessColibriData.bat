1cd76c9d5eaf2c884dd6121292c02e0d7cecd6d7
pquigle <113115134+pquigle@users.noreply.github.com>
2023-06-30 15:14:10 -0400

    Update ProcessColibriData.bat

--- PipelineAutomation/ProcessColibriData.bat
+++ PipelineAutomation/ProcessColibriData.bat
@@ -1,5 +1,6 @@
 :: call %userprofile%\anaconda3\Scripts\activate.bat
 cd %userprofile%\Documents\GitHub\ColibriObservatory\PipelineAutomation
-python processdata.py
+:: python processdata.py
+python pipeline_automation.py
 :: python processdata.py 2> D:\Logs\Pipeline\%date%.log
 pause
