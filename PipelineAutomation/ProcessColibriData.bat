28fe77c39a0673228a3b61a92d400328dcdf02ee
mjmazur <mmazur5@uwo.ca>
2023-07-30 02:17:16 +0000

    Stop from saving pipeline output to a specific file

--- PipelineAutomation/ProcessColibriData.bat
+++ PipelineAutomation/ProcessColibriData.bat
@@ -1,6 +1,7 @@
 :: call %userprofile%\anaconda3\Scripts\activate.bat
 cd %userprofile%\Documents\GitHub\ColibriObservatory\PipelineAutomation
 :: python processdata.py
-python pipeline_automation.py > D:\Logs\Pipeline\%date%.log 2>&1
+python pipeline_automation.py
+:: python pipeline_automation.py > D:\Logs\Pipeline\%date%.log 2>&1
 :: python processdata.py 2> D:\Logs\Pipeline\%date%.log
 pause
