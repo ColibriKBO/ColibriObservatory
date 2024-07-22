e0c29f541807b1004d2f6f503d839798a8d32893
ConnorMartz <cmartz@uwo.ca>
2024-07-22 15:45:53 +0000

    Changes to weather script for testing

--- Weather/WeatherPlotter/WeatherPlot.bat
+++ Weather/WeatherPlotter/WeatherPlot.bat
@@ -1,2 +1,2 @@
 call %userprofile%\Anaconda3\Scripts\activate.bat
-python %userprofile%\Documents\GitHub\ColibriObservatory\Weather\WeatherPlotter\weatherplot.py
+python %userprofile%\Documents\GitHub\ColibriObservatory\Weather\WeatherPlotter\weatherplot_new.py
