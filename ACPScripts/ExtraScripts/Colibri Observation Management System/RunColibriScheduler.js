65d3166fe60ae0cfe196653db865ecba4429250b
pquigle <pquigle@uwo.ca>
2024-10-24 01:34:52 +0000

    Update RunColibriScheduler.js

--- ACPScripts/ExtraScripts/Colibri Observation Management System/RunColibriScheduler.js
+++ ACPScripts/ExtraScripts/Colibri Observation Management System/RunColibriScheduler.js
@@ -1007,7 +1007,7 @@ function adjustPointing(ra, dec) {
     Console.PrintLine("== Pointing Correction ==");
     ts.WriteLine(Util.SysUTCDate + " INFO: == Pointing Correction ==");
     var SH = new ActiveXObject("WScript.Shell");
-    var BS = SH.Exec("python C:\\Users\\BlueBird\\Documents\\GitHub\\ColibriObservatory\\ACPScripts\\ExtraScripts\\astrometry_correction.py " + ra + " " + dec);
+    var BS = SH.Exec("python ExtraScripts\\astrometry_correction.py " + ra + " " + dec);
     var python_output = "";
     var python_error = "";
 
