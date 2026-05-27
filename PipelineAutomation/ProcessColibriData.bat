b2420d8016cfa497530c1bf23438bf5cd47b262c
Corrado777 <agirmen@uwo.ca>
2026-05-27 16:19:24 -0400

    testing bat for new pipeline

--- PipelineAutomation/ProcessColibriData.bat
+++ PipelineAutomation/ProcessColibriData.bat
@@ -1,5 +1,6 @@
 :: call %userprofile%\anaconda3\Scripts\activate.bat
 cd %userprofile%\Documents\GitHub\ColibriObservatory\PipelineAutomation
-python pipeline_automation.py
+set COLIBRI_ENV=real
+python pipeline_automation.py --env real
 :: python pipeline_automation.py > D:\Logs\Pipeline\%date%.log 2>&1
 pause
